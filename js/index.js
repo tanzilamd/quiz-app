ec0c91f89a03788259cb863039cf9a00698a2605
Tanzil <mdtanzil858@gmail.com>
2021-11-11 18:26:44 +0600

    2nd page exit btn func added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const startQuizBtn = document.querySelector(".btn button");
 const rules = document.querySelector('.rulesBox');
+const exitBtn = document.querySelector(".exitBtn");
 
 startQuizBtn.onclick = () => {
     rules.classList.add("activeInfo");
     // rules.setAttribute("style", "opacity: 1; z-index: 59999; pointer-events: auto;");
+}
+
+exitBtn.onclick = () => {
+    rules.classList.remove("activeInfo");
 }
